3ad28a190bc8be6d73b53e7eeeadd8556a280422
hardiarno <hardiarno@gmail.com>
2019-10-19 21:20:34 +0700

    add Hardi (195410093)

---
+++ Index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
+echo "<h3>Rizal (195410192)</h3>"
+echo "<h3>saya tambah lagi</h3>"
+echo "<h3>Hardi (195410093)</h3>"
+?>
