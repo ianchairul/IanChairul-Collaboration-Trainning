1878257987647e4c144e983243db22f6480307fb
rizalsamian <rizalanaksamian@gmail.com>
2019-10-19 22:40:10 +0700

    saya menghapus php kecuali file png tokopedia

--- Index.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
-echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Rizal (195410192)</h3>"
-echo "<h3>saya tambah lagi</h3>"
-echo "<h3>Hardi (195410093)</h3>"
-?>
